2490c5ed71789a63d135fffc93afa1d00ef0b92e
carlossilvestri <carlossilvestri9@gmail.com>
2022-11-07 22:45:08 -0400

    Update readme

--- src/cruise/cruise.module.ts
+++ src/cruise/cruise.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CruiseController } from './cruise.controller';
 import { CruiseService } from './cruise.service';
-
+import Puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 @Module({
   controllers: [CruiseController],
-  providers: [CruiseService],
+  providers: [
+    CruiseService,
+    {
+      provide: 'PuppeteerStealth',
+      useValue: Puppeteer.use(StealthPlugin()),
+    },
+  ],
 })
 export class CruiseModule {}
--- src/cruise/cruise.service.ts
+++ src/cruise/cruise.service.ts
@@ -1,25 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import * as puppeteer from 'puppeteer';
+import { Injectable, Inject } from '@nestjs/common';
 import * as cheerio from 'cheerio';
 import { Cruise } from './interfaces';
-
+import { Browser } from 'puppeteer-extra-plugin/dist/puppeteer';
+import { PuppeteerExtra } from 'puppeteer-extra';
+//require executablePath from puppeteer
+import { executablePath } from 'puppeteer';
 @Injectable()
 export class CruiseService {
+  constructor(
+    @Inject('PuppeteerStealth')
+    private readonly puppeteer: PuppeteerExtra,
+  ) {}
+  private browser: Browser;
   async findAll() {
     const hostname = 'https://www.carnival.com';
     const URL = `${hostname}/cruise-ships.aspx`;
-    const browser = await puppeteer.launch();
+
+    const browser = await this.puppeteer.launch({
+      executablePath: executablePath(),
+      args: [
+        '--disable-gpu',
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+        '--no-zygote',
+      ],
+    });
     const page = await browser.newPage();
     // Configure the navigation timeout
     await page.setDefaultNavigationTimeout(0);
     await page.goto(URL);
     const html = await page.evaluate(() => {
-      return document.body.innerHTML;
+      const elements = document.body.innerHTML;
+      return elements;
     });
     const $ = cheerio.load(html);
     // Arrays
     const cruises: Cruise[] = [];
-    const sailsTo: string[] = [];
+    const extensions: string[] = [];
     const sailsFrom: string[] = [];
     const durations: string[] = [];
     const images: string[] = [];
@@ -33,7 +50,7 @@ export class CruiseService {
       let sentence: string = $(this).text();
       if (sentence.includes(sailTo)) {
         sentence = sentence.replace(`${sailTo} `, '');
-        sailsTo.push(sentence);
+        extensions.push(sentence);
       }
       if (sentence.includes(sailFrom)) {
         sentence = sentence.replace(`${sailFrom} `, '');
@@ -59,7 +76,7 @@ export class CruiseService {
     for (let index = 0; index < titles.length; index++) {
       const obj: Cruise = {
         title: titles[index],
-        sail_to: sailsTo[index],
+        sail_to: extensions[index],
         sail_from: sailsFrom[index],
         duration: durations[index],
         image: images[index],
